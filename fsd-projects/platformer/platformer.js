4881b7e6416e854611e49d8552590fc1205c162f
MKalmbach <101940644+MKalmbach@users.noreply.github.com>
2025-06-26 16:48:19 +0000

    Finalized Platformer Game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -32,17 +32,45 @@ $(function () {
 
     // TODO 2 - Create Platforms
 
-
-
+    createPlatform(150,  640, 100, 10, "red");
+    createPlatform(400,  525, 100, 10, "orange");
+    createPlatform(600,  390, 100, 10, "yellow");
+    createPlatform(450,  251, 100, 10, "green");
+    createPlatform(900,  605, 100, 10, "blue");
+    createPlatform(1040, 470, 100, 10, "purple");
+    
 
     // TODO 3 - Create Collectables
 
+    // "database", "diamond", "grace", "kennedi", "max", and "steve"
 
 
+
+    createCollectable("steve", 180, 170, 0.8, 0.5);
     
+    createCollectable("grace", 410, 170, 0.8, 0.5);
+
+    createCollectable("database", 440, 170, 0.3, 0.7);
+    createCollectable("diamond",  480, 170, 0.5, 0.6);
+    createCollectable("database", 520, 170, 0.3, 0.7);
+    
+    createCollectable("max", 660, 170, 0.3, 0.7);
+
+    createCollectable("database", 890, 170, 0.3, 0.6);
+    createCollectable("kennedi",  930, 170, 0.5, 0.6);
+    createCollectable("database", 975, 170, 0.3, 0.6);
+
+    createCollectable("database", 1025, 170, 0.3, 0.6);
+    createCollectable("database", 1070, 170, 0.3, 0.5);
+    createCollectable("database", 1115, 170, 0.3, 0.6);
+
+
+
     // TODO 4 - Create Cannons
 
-
+    createCannon("top",    190,  2000);
+    createCannon("right",  340,  1800);
+    createCannon("bottom", 740,  1500);
     
     
     //////////////////////////////////
